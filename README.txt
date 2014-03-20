abf636ae192735425191beda013aa9be37398c0a
delrayva <delrayva@hotmail.com>
2014-03-20 10:51:22 -0400

    Just trying a branch update
    Added some text to README.txt and created trythis,py

--- README.txt
+++ README.txt
@@ -1,1 +1,3 @@
 There's nothing useful in here.  I'm just experimenting with github, and using this space to try things.
+
+This is a small add to see what happens to the file.
---
+++ trythis.py
@@ -0,0 +1,11 @@
+# What happens if I put some python code here?
+
+def main():
+	""" Nothing to see here, just some trial code
+	"""
+	for i in range(0,7):
+		print "%d\t" % i,
+	print "\n"
+
+if __name__ == "__main__":
+	main()
